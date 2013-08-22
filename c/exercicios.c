b33774dcf73340e50801beec7f086d7832d1a1c8
Reinaldo A. C. Rauch <reinaldorauch@gmail.com>
2013-08-22 12:05:33 -0300

    Adicionado .gitignore e implementada função para inverter linhas

--- c/exercicios.c
+++ c/exercicios.c
@@ -1,7 +1,7 @@
 /**
  *  1) Escreva um programa em linguagem C que execute as seguintes operações:
  *  a) Declare uma matriz quadrada, com dimensões 4x4, de valores inteiros, inicializada com
- *	os valores de 01 a 16
+ *  os valores de 01 a 16
  *  b) Exiba a matriz, e a seguir, faça as seguintes trocas de conteúdos:
  *  c) Conteúdos da 2º linha com os da 4º Linha
  *  d) Conteúdos da 3º coluna com os da 4º coluna
@@ -10,138 +10,187 @@
  *  g) Exibir a matriz modificada
  */
 #include <stdlib.h>
-#include <strings.h>
+#include <string.h>
 #include <stdio.h>
 
 #define MAX 4
 
 /**
- * Preenche matriz
+ * Imprime matriz
  */
-void preenche_matriz(int matriz[][]) {
+void imprime_matriz(int matriz[MAX][MAX]) {
+    int i, j;
 
+    puts("\n\t+----+----+----+----+----+");
+    puts("\t|    |  1 |  2 |  3 |  4 | <-- Colunas");
+    puts("\t+----+----+----+----+----+");
+
+    for(i = 0; i < MAX; i++) {
+        printf("\t| %2d |", i + 1);
+        for(j = 0; j < MAX; j++) {
+            if(!matriz[i][j])
+                printf("  ");
+            else
+                printf("%3d", matriz[i][j]);
+            printf(" |");
+        }
+        putchar('\n');
+        puts("\t+----+----+----+----+----+");
+    }
+
+    puts("\t  |");
+    puts("\t  +---- Linhas");
+
+    putchar('\n');
 }
 
 /**
  * Imprime cabeçalho
  */
-void cabecalho(int matriz[][]) {
+void cabecalho(int matriz[MAX][MAX]) {
 
-	var i, j;
+    int i, j;
 
-	puts("+--------------------------------------------------------------------+");
-	puts("|         Exercícios do dia 20/08/13                                 |");
-	puts("+--------------------------------------------------------------------+");
-	puts("\n\t+---+---+---+---+---+");
-	puts("\t|   | 1 | 2 | 3 | 4 | <-- Colunas");
-	puts("\t+---+---+---+---+---+");
+    puts("+--------------------------------------------------------------------+");
+    puts("|         Exercícios do dia 20/08/13                                 |");
+    puts("+--------------------------------------------------------------------+");
+    
+    imprime_matriz(matriz);
 
-	for(i = 0; i < MAX; i++) {
-		printf("\t|   | ");
-		for(j = 0; j < MAX; j++) {
-			if(!matriz[i][j])
-				putchar(' ');
-			else
-				printf("%d", matriz[i][j]);
-			prints(" |");
-		}
-		putchar('\n');
-		puts("\t+---+---+---+---+---+");
-	}
-
-	putchar('\n');
-
-	puts("+--------------------------------------------------------------------+");
-
-}
-
-/**
- * verifica se a matriz passada tem espaços vazios
- */
-int tem_espacos_vazios(int matriz[][]) {
-	int i, j;
-
-	for (int i = 0; i < MAX; i++)
-	{
-		for (int j = 0; j < MAX; j++)
-		{
-			if(!matriz[i][j])
-				return 1;
-		}
-	}
+    puts("+--------------------------------------------------------------------+");
 
 }
 
 /**
  *  Preenche a matriz com valores de 1 a 16
  */
-void fill_matrix(int matriz[][]) {
-	int i, j, count = 1;
+void fill_matrix(int matriz[MAX][MAX]) {
+    int i, j, count = 1;
 
-	for (int i = 0; i < MAX; i++)
-	{
-		for (int j = 0; j < MAX; j++)
-		{
-			matriz[i][j] = count++;
-		}
-	}
+    for (i = 0; i < MAX; i++)
+    {
+        for (j = 0; j < MAX; j++)
+        {
+            matriz[i][j] = count++;
+        }
+    }
 }
 
 /**
  * Sai do programa
  */
 void encerra() {
-	puts("\tSaindo...");
-	exit(0);
+    puts("\tSaindo...");
+    exit(0);
 }
 
 /**
  * Para o menu
  */
 void para_menu() {
-	char c;
-	puts("\tPressione enter para continuar");
-	getchar();
-	while((c = getchar()) != \n && c != EOL);
+    char c;
+    puts("\tPressione enter para continuar");
+    getchar();
+    while((c = getchar()) != '\n');
+}
+
+/**
+ * Valida linhas, colunas
+ */
+int valida_parametros(int param1, int param2) {
+    int cond1 = (param1 > MAX || param2 > MAX || param1 < 0 || param2 < 0),
+        cond2 = (param1 == param2);
+    if(cond1) {
+        puts("Parâmetros que acessam posições fora da matriz inserida");
+        return 0;
+    } else if(cond2) {
+        puts("Não é possível inverter as mesmas linhas ou não faz diferença");
+        return 0;
+    } else
+        return 1;
+    
+}
+
+/**
+ * Inverte linhas da matriz
+ *
+ */
+int inverte_linhas(int linha1, int linha2, int matriz[MAX][MAX]) {
+
+    if(!valida_parametros(linha1, linha2))
+        return 0;
+
+    linha1--;
+    linha2--;
+
+    printf("Iniciando a inversão das linhas nº %d com a nº %d\n", linha1, linha2);
+
+    int aux, i;
+
+    for(i = 0; i < MAX; i++) {
+        aux = matriz[linha1][i];
+        matriz[linha1][i] = matriz[linha2][i];
+        matriz[linha2][i] = aux;
+    }
+
+    puts("\n\tResultado da inversão\n");
+
+    imprime_matriz(matriz);
+
+    return 1;
+
+}
+
+/**
+ * Inverte colunas
+ */
+int inverte_colunas(int coluna1, int coluna2, int matriz[MAX][MAX]) {
+
 }
 
 /**
  * Programa
  */
-void programa(int matriz[][]) {
-	puts("IÉIÉ, pegadinha do malandro");
+void programa(int matriz[MAX][MAX]) {
+    /*puts("IÉIÉ, pegadinha do malandro");*/
+
+    inverte_linhas(2, 4, matriz);
+
+
+
 }
+
 
 int main() {
 
-	int matriz[MAX][MAX];
-	char c;
+    int matriz[MAX][MAX];
+    char c;
 
-	fill_matrix(matriz);
+    fill_matrix(matriz);
 
-	while(1) {
+    while(1) {
 
-		system("clear");
+        system("clear");
 
-		cabecalho(matriz);
+        cabecalho(matriz);
 
-		puts("\t Escolha uma opção:\n");
-		puts("\t[ 1 ] Realiza programa");
-		puts("\t[ 2 ] Sair do programa");
+        puts("\t Escolha uma opção:\n");
+        puts("\t[ 1 ] Realiza programa");
+        puts("\t[ 2 ] Sair do programa");
 
-		c = getchar();
+        c = getchar();
 
-		switch (c) {
-		case '1':
-			programa(matriz);
-			break;
-		case '2':
-			encerra();
-			break;
-		}
+        switch (c) {
+        case '1':
+            programa(matriz);
+            break;
+        case '2':
+            encerra();
+            break;
+        }
 
-		para_menu();
-		
-	}
+        para_menu();
+        
+    }
 
 }
